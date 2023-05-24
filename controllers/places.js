c0cdfc686209902996c0bf4e783b4f31f28b723e
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-24 19:26:47 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -3,6 +3,18 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
   res.render('places/index')
 })
+
+router.get('/:id', (req, res) => {
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
 // wait
 
 module.exports = router
