1cb0b383128e0b61aa4b33b2075360d7a1e1b12a
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-25 16:48:01 -0400

    jazzy form page

--- views/home.jsx
+++ views/home.jsx
@@ -13,7 +13,7 @@ function home () {
         </div>
       </div>
       <a href="/places">
-        <button className="btn-primary">Places Page</button>
+        <button className="btn btn-primary">Places Page</button>
       </a>
     </main>
   </Def>
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form() {
     <Def>
       <main>
         <h1>Add a New Place</h1>
+        <form method="POST" action="/places">
+          <div>
+            <label htmlFor="name">Place Name</label>
+            <input className="form control" id="name" name="name" required />
+          </div>
+          <div>
+            <label htmlFor="pic">Place Picture</label>
+            <input className="form control" id="pic" name="pic" />
+          </div>
+          <div>
+            <label htmlFor="city">Place City</label>
+            <input className="form control" id="city" name="city" />
+          </div>
+          <div>
+            <label htmlFor="state">Place State</label>
+            <input className="form control" id="state" name="state" />
+          </div>
+          <div>
+            <label htmlFor="cuisines">Place Cuisines</label>
+            <input className="form control" id="cuisines" name="cuisines" required />
+          </div>
+            <input className="btn btn-primary" type="submit" value="Add Place" />
+        </form>
       </main>
     </Def>
   )
