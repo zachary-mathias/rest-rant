c7903411113563dfcc53f3dfae398045c80e9885
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-25 16:20:42 -0400

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,25 @@
 const router = require('express').Router()
 
+let places = [{
+  name: 'Sabaidee Thai & Sushi Bar',
+  city: 'concord',
+  state: 'NC',
+  cuisines: 'Thai, Pan-Asian, sushi',
+  pic: '/images/thai.jpeg'
+}, {
+  name: 'Mac Tabby Cat Cafe',
+  city: 'charlotte',
+  state: 'NC',
+  cuisines: 'Coffee, Bakery',
+  pic: '/images/cat-cafe.jpeg'
+}]
+
 router.get('/', (req, res) => {
   res.render('places/index')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
