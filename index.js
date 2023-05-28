098dbdced1460ca54f0b8f568c525032672c6d90
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-05-28 14:55:31 -0400

    added mongoose and made pages that aren't rendering have to look into it

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 
+
 // Express settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,27 +7,31 @@ function new_form() {
       <main>
         <h1>Add a New Place</h1>
         <form method="POST" action="/places">
-          <div>
+          <div className='form-group'>
             <label htmlFor="name">Place Name</label>
             <input className="form control" id="name" name="name" required />
           </div>
-          <div>
+          <div className='form-group'>
             <label htmlFor="pic">Place Picture</label>
             <input className="form control" id="pic" name="pic" />
           </div>
-          <div>
+          <div className='form-group'>
             <label htmlFor="city">Place City</label>
             <input className="form control" id="city" name="city" />
           </div>
-          <div>
+          <div className='form-group'>
             <label htmlFor="state">Place State</label>
             <input className="form control" id="state" name="state" />
           </div>
-          <div>
+          <div className='form-group'>
             <label htmlFor="cuisines">Place Cuisines</label>
             <input className="form control" id="cuisines" name="cuisines" required />
           </div>
             <input className="btn btn-primary" type="submit" value="Add Place" />
+          <div className="form-group">
+            <label for="founded">Founded Year</label>
+            <input className="form-control" id="founded" name="founded" />
+          </div>
         </form>
       </main>
     </Def>
